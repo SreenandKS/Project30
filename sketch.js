23bb7a519d56ac1841778d4afcb65891ead362d5
SreenandKS <78798957+SreenandKS@users.noreply.github.com>
2021-03-06 15:24:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,55 +12,56 @@ var polygon,slingshot;
 
 
 function setup(){
-    var canvas = createCanvas(800,700);
+    var canvas = createCanvas(900,400);
     engine = Engine.create();
     world = engine.world;
 
     ground = new Ground(0,690,1600,10);
-    stand1 = new Ground(390,600,230,10);
-    stand2 = new Ground(600,300,170,10);
+    stand1 = new Ground(750,180,230,10);
+    stand2 = new Ground(520,300,170,10);
 
-    b1 = new Box(300,570,30,40);
-    b2 = new Box(330,570,30,40);
-    b3 = new Box(360,570,30,40);
-    b4 = new Box(390,570,30,40);
-    b5 = new Box(420,570,30,40);
-    b6 = new Box(450,570,30,40);
-    b7 = new Box(480,570,30,40);
+    fill("blue");
+    b1 = new Box(660,150,30,40);
+    b2 = new Box(690,150,30,40);
+    b3 = new Box(720,150,30,40);
+    b4 = new Box(750,150,30,40);
+    b5 = new Box(780,150,30,40);
+    b6 = new Box(810,150,30,40);
+    b7 = new Box(840,150,30,40);
 
     fill("red");
-    b8 = new Box(330,530,30,40);
-    b9 = new Box(360,530,30,40);
-    b10 = new Box(390,530,30,40);
-    b11 = new Box(420,530,30,40);
-    b12 = new Box(450,530,30,40);
+    b8 = new Box(690,110,30,40);
+    b9 = new Box(720,110,30,40);
+    b10 = new Box(750,110,30,40);
+    b11 = new Box(780,110,30,40);
+    b12 = new Box(810,110,30,40);
 
     fill("yellow");
-    b13 = new Box(360,490,30,40);
-    b14 = new Box(390,490,30,40);
-    b15 = new Box(420,490,30,40);
+    b13 = new Box(720,70,30,40);
+    b14 = new Box(750,70,30,40);
+    b15 = new Box(780,70,30,40);
 
     fill("green")
-    b16 = new Box(390,450,30,40);
+    b16 = new Box(750,30,30,40);
 
     fill("red");
-    b17 = new Box(540,270,30,40);
-    b18 = new Box(570,270,30,40);
-    b19 = new Box(600,270,30,40);
-    b20 = new Box(630,270,30,40);
-    b21 = new Box(660,270,30,40);
+    b17 = new Box(460,270,30,40);
+    b18 = new Box(490,270,30,40);
+    b19 = new Box(520,270,30,40);
+    b20 = new Box(550,270,30,40);
+    b21 = new Box(580,270,30,40);
 
     fill("yellow");
-    b22 = new Box(570,230,30,40);
-    b23 = new Box(600,230,30,40);
-    b24 = new Box(630,230,30,40);
+    b22 = new Box(490,230,30,40);
+    b23 = new Box(520,230,30,40);
+    b24 = new Box(550,230,30,40);
 
     fill("green");
-    b25 = new Box(600,190,30,40);
+    b25 = new Box(520,190,30,40);
 
     polygon = new Polygon(50,200,50);
 
-    slingshot = new SlingShot(polygon.body,{x:60,y:600});
+    slingshot = new SlingShot(polygon.body,{x:130,y:200});
 
 }
 
@@ -68,6 +69,7 @@ function draw(){
     background(255,144,84);
     Engine.update(engine);
 
+   
     ground.display();
     stand1.display();
     stand2.display();
@@ -99,11 +101,13 @@ function draw(){
     polygon.display();
     slingshot.display();
 
-    polygon.debug = true;
-    
-    fill(0);
+    fill(0,236,83);
     textSize(20);
     text("Drag the hexagonal stone and release it, to launch it towards the blocks",100,20);
+
+    polygon.debug = true;
+
+   
 }
 
 function mouseDragged(){
